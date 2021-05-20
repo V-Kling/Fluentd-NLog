bfca652e9397f151f4cd1294864aaaa560b2fe1c
V-Kling <59062384+V-Kling@users.noreply.github.com>
2021-05-20 15:53:11 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Threading;
+using System;
+using Serilog;
+
+namespace Monitoring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        var log = new LoggerConfiguration().WriteTo.Fluentd("localhost",24224,"Monitoring.Code",Serilog.Events.LogEventLevel.Debug).CreateLogger();
+
+log.Information($"That's it!");
+
+            int counter = 0;
+
+            while(true)
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Log information via Console: " + counter);
+                Thread.Sleep(2000);
+                log.Debug($"That's it!");
+                counter++;
+            }
+        }
+    }
+}
